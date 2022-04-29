978217820a6ba11043e26af5a8e1d0937c1cfcb5
Lucas-Esteban-Rodriguez <eteban2412@gmail.com>
2022-04-29 17:42:58 -0300

    Avances en contacto

--- contacto.js
+++ contacto.js
@@ -1,9 +1,12 @@
 const inputRadio = document.getElementById('inputRadio')
 const botonesFormulario = document.getElementById('a')
+function preventDefault(e) {
+    e.target.preventDefault
+}
 inputRadio.onchange = () => {
     if (inputRadio.checked) {
-        botonesFormulario.innerHTML = '<button class="btn btn-success">Siguiente</button> <button class="btn btn-danger">Reiniciar</button>'
+        botonesFormulario.innerHTML = '<button type="button" onclick="preventDefault(e)" class="btn btn-success">Siguiente</button> <button type="reset" onclick="preventDefault(e)" class="btn btn-danger">Reiniciar</button>'
     }else {
-        botonesFormulario.innerHTML = '<button class="btn btn-primary">Enviar</button><button class="btn btn-danger">Reiniciar</button>'
+        botonesFormulario.innerHTML = '<button type="submit" onclick="preventDefault(e)" class="btn btn-primary">Enviar</button><button type="reset" onclick="preventDefault(e)" class="btn btn-danger">Reiniciar</button>'
     }
 }
